0618aa9b5d030da0a6f738ada9fd029ba1baa98e
David Ledvinka <david.ledvinka@post.cz>
2019-04-29 09:57:09 +0200

    'initial'

---
+++ TurnOffLANSettingsAutodetection.Service/LANSettingsGuard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Microsoft.Win32;
+
+namespace TurnOffLANSettingsAutodetection.Service
+{
+    public class LANSettingsGuard
+    {
+        int changedCounter = 0;
+        readonly Timer _timer;
+        public LANSettingsGuard()
+        {
+            _timer = new Timer(5000) { AutoReset = true };
+            _timer.Elapsed += OnTimerElapsed;
+        }
+
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                using (RegistryKey RegKey = Registry.CurrentUser.OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections", true))
+                {
+                    byte[] defConnection = (byte[])RegKey.GetValue("DefaultConnectionSettings");
+
+                    if (defConnection[8] != 1)
+                    {
+                        defConnection[8] = 1;
+                        Console.WriteLine($"Registry value changed at {DateTime.Now}, counter = {++changedCounter}");
+                    }
+
+                    RegKey.SetValue("DefaultConnectionSettings", defConnection);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ TurnOffLANSettingsAutodetection.Service/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TurnOffLANSettingsAutodetection.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<LANSettingsGuard>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => new LANSettingsGuard());                //3
+                    s.WhenStarted(tc => tc.Start());                         //4
+                    s.WhenStopped(tc => tc.Stop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("Description is empty");                   //7
+                x.SetDisplayName("ABB.LANSettingsGuard");                                  //8
+                x.SetServiceName("ABB.LANSettingsGuard");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
